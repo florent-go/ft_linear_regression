c0bd09eee4c184be4a07516be5ecbfa243b5ffbe
Florent Gomez <fgomez@c2r4p8.42nice.fr>
2022-06-21 15:16:31 +0200

    linear_regression  good

---
+++ estimation.py
@@ -0,0 +1,16 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+def getThetas():
+    t0, t1 = 0
+
+
+def main():
+    thetha = getThetas()
+    getKilometrage()
+    print(kilometrage)
+    print(prix)
+
+if __name__ == '__main__':
+    main()
---
+++ linear_regression.py
@@ -0,0 +1,48 @@
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv("data.csv")
+
+# def erreur_quadratique(m, b, points):
+#     total_error = 0
+#     for i in range(len(points)):
+#         x = points.iloc[i].km
+#         y = points.iloc[i].price
+#         total_error += (y - (m * x + b)) ** 2
+#     total_error / float(len(points))
+
+def gradient_descent(m_now, b_now, points, L):
+    m_gradient = 0
+    b_gradient = 0
+
+    n = len(points)
+
+    for i in range(n):
+        x = points.iloc[i].km
+        y = points.iloc[i].price
+
+        m_gradient += -(2/n) * x * (y - (m_now * x + b_now))
+        b_gradient += -(2/n) * (y - (m_now * x + b_now))
+    
+    m = m_now - m_gradient * L
+    b = b_now - b_gradient * L
+    return m, b
+
+m = 0
+b = 0
+L = 0.01
+iteration = 4000
+points = (data - data.min())/ (data.max() - data.min())
+print(points)
+for i in range(iteration):
+    if i % 50 == 0:
+        print(f"iteration: {i}")
+        print("b value = ", b)
+        print("m value = ", m)
+    m, b = gradient_descent(m, b, points, L)
+
+plt.scatter(points.km, points.price, color="black")
+plt.plot(list(range(0, 2)), [m * x + b for x in range(0, 2)], color="red")
+plt.show()
